9979898c5018f3eeb099961e7e5cf1da4e8eb6a1
cofprophecy <166811971+cofprophecy@users.noreply.github.com>
2024-09-02 15:41:20 +0000

    2

---
+++ konoha/02_data_types.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs,not browser
+
+console.log(3+3)
+console.log("k07");
+
+
+let name = "sasuke"
+let age = 19
+let is_logged_in = false
+let state //undefined
+let state2 = '' // kinda like 0
+
+//1. primitive datatypes
+// number => 2 to the power 53
+// bigint (for greater than above number)
+// string => ""
+// boolean => true/false
+// null => standalone value (empty)
+// undefined => value unassigned
+// symbol => unique
+
+//2. object
+
+console.log(typeof 'ks')
+console.log(typeof age )
+console.log(typeof null) //output type is object
+console.log(typeof undefined) //output type is undefined
+
+
