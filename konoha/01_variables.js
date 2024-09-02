7d36f348f1e15fac5df4ccbfe56390e948da0906
cofprophecy <166811971+cofprophecy@users.noreply.github.com>
2024-09-02 15:05:57 +0000

    2

---
+++ konoha/01_variables.js
@@ -0,0 +1,22 @@
+console.log("kakashi")
+const account_id = 12345
+let account_email = 'konoha@gmail.com'
+var account_password = '15874'
+account_city = 'leaf'
+
+let account_state     
+
+// account_id = 2    // not allowed
+
+account_email = "hc23er@gmail.com"
+account_password = '123455'
+account_city = 'sand'
+console.log(account_id);
+
+/*
+prefer not to use var
+because of issue in block and functional scope
+*/
+
+
+console.table([account_id,account_email,account_password,account_city,account_state])
